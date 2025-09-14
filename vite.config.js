d65090ecc246e5c0b005b6814bea08e1ea3efdf5
KrystekInformatyk <144493310+KrystekInformatyk@users.noreply.github.com>
2025-09-14 18:57:41 +0200

    Init

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // NAZWA repozytorium w GitHub Pages:
-  base: '/portfolio-react/',
+  base: '/portfolio-react/', // ← NAZWA_REPO
 })
